4ca7fff9f6c77c55aae5eae81aba72503ddbceee
aki-ika <76543112+aki-ika@users.noreply.github.com when>
2023-11-23 21:24:15 +0900

    Add basePath and experimental images configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "",
   reactStrictMode: true,
+  experimental: {
+    images: {
+      unoptimized: true
+    }
+  },
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
